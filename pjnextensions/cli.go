7e189ebe18c9f0de2efcaa72c7caacdb65a9b807
niteshswarnakar <nitesh.swarnakar1@gmail.com>
2025-11-12 22:13:08 +0545

    Update root directory for extensions

--- pjnextensions/cli.go
+++ pjnextensions/cli.go
@@ -1,25 +1,26 @@
 package pjnextensions
 
 import (
-	"github.com/spf13/cobra"
 	"path"
+
+	"github.com/spf13/cobra"
 )
 
 func Setup(extensionID string) {
 	onceApp.Do(func() {
 		rCmd := GetRootCmd()
 		rCmd.PersistentFlags().Bool("prod", false, "Is production?")
-		rCmd.PersistentFlags().StringP("root", "r", "/ros/server/extensions", "Root Directory (default /ros/server/extensions)")
+		rCmd.PersistentFlags().StringP("root", "r", "../../", "Root Directory (default ../)")
 		rCmd.PersistentPreRunE = func(cmd *cobra.Command, args []string) error {
 			isProd, _ := cmd.Flags().GetBool("prod")
-			rootDir := "./"
+			rootDir := "../../"
 			if isProd {
 				rootDir, _ = cmd.Flags().GetString("root")
 			}
 			extension = &Extension{
 				ID:           extensionID,
 				RootDir:      rootDir,
-				ExtensionDir: path.Join(extensionID),
+				ExtensionDir: path.Join(extensionID, "ext"),
 				DataDir:      path.Join(extensionID, "data"),
 			}
 			return nil
