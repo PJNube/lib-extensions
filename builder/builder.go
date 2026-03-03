d5f84aefea28ad81294a0d89bf65f0d71f469342
Nitesh Swarnakar <45380458+niteshswarnakar@users.noreply.github.com>
2026-03-03 14:36:21 +0530

    Pack config also in extension library builder (#12)
    * Pack config also in extension library builder
    
    * Add builder opts
    
    * Minor improvements
    
    ---------
    
    Co-authored-by: enju <enjuthulung@gmail.com>

---
+++ builder/builder.go
@@ -0,0 +1,73 @@
+package builder
+
+import (
+	"archive/zip"
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+	"time"
+)
+
+type Opts struct {
+	Architecture string
+	ConfigDir    string
+}
+
+func addFilesToZip(zipWriter *zip.Writer, paths ...string) error {
+	for _, path := range paths {
+		err := addFileToZip(zipWriter, path)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func addFileToZip(zipWriter *zip.Writer, filePath string) error {
+	fileInfo, err := os.Stat(filePath)
+	if os.IsNotExist(err) && filepath.Ext(filePath) != ".exe" {
+		filePath += ".exe"
+		fileInfo, err = os.Stat(filePath)
+	}
+
+	if os.IsNotExist(err) {
+		return fmt.Errorf("file does not exist: %s", filePath)
+	}
+
+	file, err := os.Open(filePath)
+	if err != nil {
+		return fmt.Errorf("failed to open file %s: %w", filepath.Base(filePath), err)
+	}
+	defer file.Close()
+
+	filename := filepath.Base(filePath)
+
+	header := &zip.FileHeader{
+		Name:   filename,
+		Method: zip.Deflate,
+	}
+
+	mode := fileInfo.Mode()
+	if mode&0111 == 0 {
+		mode |= 0755
+	}
+	header.SetMode(mode)
+
+	writer, err := zipWriter.CreateHeader(header)
+	if err != nil {
+		return fmt.Errorf("failed to create zip entry for %s: %w", filename, err)
+	}
+
+	_, err = io.Copy(writer, file)
+	if err != nil {
+		return fmt.Errorf("failed to copy file %s to zip: %w", filename, err)
+	}
+
+	return nil
+}
+
+func getBuildTime() string {
+	currentTime := time.Now().UTC()
+	return currentTime.Format(time.RFC3339)
+}
